886045866ec5a1e19e311d3f1894898d039609a2
Adi Luthfi Nur Roki <adiluthfi03@gmail.com>
2022-11-24 16:21:48 +0700

    add class node and declarate variable

--- Exercise Linked List B/Program.cs
+++ Exercise Linked List B/Program.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Exercise_Linked_List_B
 {
+    class Node
+    {
+        /*creates Nodes for the circular nexted list*/
+        public int rollNumber;
+        public string name;
+        public Node next;
+    }
     internal class Program
     {
         static void Main(string[] args)
